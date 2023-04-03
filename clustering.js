6c90b2c6616c105a9c679ed50bd3bdf91872e4e3
Andrey Alibekov <alibekovand@gmail.com>
2023-04-03 13:46:06 +0700

    implemented k-means

--- clustering.js
+++ clustering.js
@@ -1,7 +1,7 @@
 const RADIUS = 7;
 const CLUSTERCOUNT = 3;
 const COLORS = ["#000000", "#FF0000", "#00FF00", "#0000FF", "#D7DF01",
- "#FF00FF", "#00FFFF", "#00FFFF", "#8A4B08", "#F5A9E1", "#A4A4A4"]
+ "#FF00FF", "#00FFFF", "#8A4B08", "#F5A9E1", "#A4A4A4"]
 // 0 - black, 1 - red, 2 - green, 3 - blue, 4 - yellow,
 // 5 - violet, 6 - cyan, 7 - brown, 8 - pink, 9 - grey
 
@@ -14,12 +14,18 @@ class Point {
 }
 
 
+function distance(p1, p2) {
+    return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
+}
+
+
 function makeRandomPoint(canvas) {
     const rect = canvas.getBoundingClientRect()
     let x = Math.floor(Math.random() * canvas.width);
     let y = Math.floor(Math.random() * canvas.height);
     return new Point(x, y, 0);
 }
+
 
 function getMousePos(canvas, e) {
     const rect = canvas.getBoundingClientRect()
@@ -44,28 +50,98 @@ function addRandomPoints(points, canvas, ammount) {
     return points;
 }
 
+
+function generateCentroids(canvas) {
+    let centroids = new Array;
+    centroids = addRandomPoints(centroids, canvas, CLUSTERCOUNT);
+    for (let i in centroids) {
+        centroids[i].cluster = i;
+    }
+    return centroids;
+}
+
+
 function drawPoint(ctx, point) {
     ctx.fillStyle = COLORS[point.cluster];
     ctx.fillRect(point.x - RADIUS/2, point.y - RADIUS/2, RADIUS, RADIUS);
 }
 
 
-function update(canvas, ctx, points, e) {
+function drawCentroid(ctx, centroid) {
+    ctx.fillStyle = COLORS[centroid.cluster];
+    ctx.fillRect(centroid.x - RADIUS, centroid.y - RADIUS/4, RADIUS*2, RADIUS/2);
+    ctx.fillRect(centroid.x - RADIUS/4, centroid.y - RADIUS, RADIUS/2, RADIUS*2);
+}
+
+
+function kMeansIteration(points, centroids) {
+    let totalDistChange = 0;
+    let clusters = new Array(CLUSTERCOUNT);
+    for (let i = 0; i < CLUSTERCOUNT; i++) {
+        clusters[i] = {sumDistX: 0, sumDistY: 0, count: 0};
+    }
+
+    for (point in points) {
+        let minDist = 10000000;
+        let minCluster = -1;
+        for (center in centroids) {
+            let dist = distance(points[point], centroids[center]);
+            if (dist < minDist) {
+                minDist = dist;
+                minCluster = center;
+            }
+        }
+        points[point].cluster = minCluster;
+        clusters[minCluster].count++;
+        clusters[minCluster].sumDistX += points[point].x;
+        clusters[minCluster].sumDistY += points[point].y;
+    }
+    for (center in centroids) {
+        let oldCentroid = JSON.parse(JSON.stringify(centroids[center])); //deep copy
+        if (clusters[center].count != 0) {
+            centroids[center].x = clusters[center].sumDistX / clusters[center].count;
+            centroids[center].y = clusters[center].sumDistY / clusters[center].count;
+            totalDistChange += distance(oldCentroid, centroids[center]);
+        }
+    }
+    return totalDistChange;
+}
+
+function kMeans(points, centroids) {
+    let distChange = 10000;
+    let iterationCount = 0;
+    while (distChange > 0.1 * CLUSTERCOUNT) {
+        iterationCount++;
+        distChange = kMeansIteration(points, centroids);
+    }
+    //console.log(iterationCount);
+}
+
+function update(canvas, ctx, points, centroids, e) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     points = addPoint(points, canvas, e);
     //points = addRandomPoints(points, canvas, 10);
+
+    kMeans(points, centroids);
+
+
     for (let indx in points) {
         drawPoint(ctx, points[indx]);
     }
+    for (let indx in centroids) {
+        drawCentroid(ctx, centroids[indx]);
+    }
 }
+
 
 
 function main() {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
     let points = new Array;
+    let centroids = generateCentroids(canvas);
     canvas.addEventListener('mousedown', function(e) {
-        update(canvas, ctx, points, e);
+        update(canvas, ctx, points, centroids, e);
     });
 
 }
