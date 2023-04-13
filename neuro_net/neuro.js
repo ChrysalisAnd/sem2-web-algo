96704a4ea33a5fe2f994e5665a1a25244e492ca4
Andrey Alibekov <alibekovand@gmail.com>
2023-04-14 02:09:48 +0700

    It's ALIVE!

--- neuro_net/neuro.js
+++ neuro_net/neuro.js
@@ -13,11 +13,16 @@ const iterations = 5;
 
 const trainingDataPath = "./MNIST/mnist_train.csv";
 const testDataPath = "./MNIST/mnist_test.csv";
+const weightsFilename = "weights.json";
+const savedWeightsPath = `./${weightsFilename}`;
 
 const trainingData = [];
 const trainingLabels = [];
 const testData = [];
 const testLabels = [];
+const savedWeights = {};
+
+const printSteps = 1000;
 
 
 class Network {
@@ -82,10 +87,10 @@ class Network {
 
 
     update = () => {
-    this.wInHidden = math.sum(this.wInHidden,
-        math.dotMultiply(this.learningRate, this.cache.input_hidden_nablaE));
-    this.wHiddenOut = math.sum(this.wHiddenOut,
-        math.dotMultiply(this.learningRate, this.cache.hidden_output_nablaE));
+    this.wInHidden = math.add(this.wInHidden,
+        math.dotMultiply(this.cache.i_h_nablaE, this.learningRate));
+    this.wHiddenOut = math.add(this.wHiddenOut,
+        math.dotMultiply(this.cache.h_o_nablaE, this.learningRate));
     };
 
 
@@ -108,15 +113,41 @@ class Network {
 window.onload = async () => {
     net = new Network(inputNodes, hiddenNodes, outputNodes, learningRate);
 
+    trainButton.disabled = true;
+    testButton.disabled = true;
+    loadWeightsButton.disabled = true;
+
+    status.innerHTML = "Loading the data sets. Please wait ...<br>";
+
     const trainCSV = await loadData(trainingDataPath, "CSV");
     if (trainCSV) {
         prepareData(trainCSV, trainingData, trainingLabels);
+        status.innerHTML += "Training data successfully loaded...<br>";
     }
 
     const testCSV = await loadData(testDataPath, "CSV");
     if (testCSV) {
         prepareData(testCSV, testData, testLabels);
+        status.innerHTML += "Test data successfully loaded...<br>";
     }
+
+    if (!trainCSV || !testCSV) {
+        status.innerHTML += "Error loading train/test data set. Check your file path.";
+        return;
+    }
+
+    trainButton.disabled = false;
+    testButton.disabled = false;
+    
+    const weightsJSON = await loadData(savedWeightsPath, "JSON");
+
+    if (weightsJSON) {
+        savedWeights.wInHidden = weightsJSON.wInHidden;
+        savedWeights.wHiddenOut = weightsJSON.wHiddenOut;
+        loadWeightsButton.disabled = false;
+    }
+
+    status.innerHTML += "Ready.<br><br>";
 };
 
 
@@ -144,6 +175,10 @@ async function loadData(path, type) {
 function prepareData(rawData, target, labels) {
     rawData = rawData.split("\n");
     rawData.pop(); // last element is empty because last line in CSV file is blank
+    /*const length = rawData.length * 9 / 10;
+    for (let i = 0; i < length; i++) {
+        rawData.pop();
+    }*/
 
     rawData.forEach((current) => {
         let sample = current.split(",").map((x) => Number(x));
@@ -158,42 +193,120 @@ function prepareData(rawData, target, labels) {
 }
 
 function train() {
+    trainButton.disabled = true;
+    testButton.disabled = true;
+    loadWeightsButton.disabled = true;
+    download.innerHTML = "";
+
     if (iter < iterations) {
         iter++;
     
         trainingData.forEach((current, index) => {
-        /* create one-hot encoding of the label */
-            const label = trainingLabels[index];
-            const oneHotLabel = Array(10).fill(0);
-            oneHotLabel[label] = 0.99;
+            setTimeout(() => {
+            /* create one-hot encoding of the label */
+                const label = trainingLabels[index];
+                const oneHotLabel = Array(10).fill(0);
+                oneHotLabel[label] = 0.99;
 
-            net.train(current, oneHotLabel);
-            if (index === trainingData.length - 1) {
-                test("", true); // true to signal "test" that it is called from within training 
-            }
+                net.train(current, oneHotLabel);
+                if (index > 0 && !((index + 1) % printSteps)) {
+                    status.innerHTML += `finished  ${index + 1}  samples ... <br>`;
+                }
+
+                /* check if the end of the training iteration is reached */
+                if (index === trainingData.length - 1) {
+                    status.innerHTML += `Loss:  ${math.sum(net.cache.loss) / trainingData.length}<br><br>`;
+                    net.cache.loss = [];
+                    test("", true); // true to signal "test" that it is called from within training 
+                }
+            }, 0);
         });
     }
 }
 
 function test(_, inTraining = false) { 
-  
-  let correctPredicts = 0;
-  
-  testData.forEach((current, index) => {
-    const actual = testLabels[index];
-    
-    const predict = formatPrediction(net.predict(current));
-    predict === actual ? correctPredicts++ : null;
+/* first parameter is for data from event listener */
 
-    /* check if training is complete */
-    /* if test is called from within training and the training is not complete yet, continue training */
-    if (index >= testData.length - 1 && inTraining) {
-      train();
-    }     
-  });
+    trainButton.disabled = true;
+    testButton.disabled = true;
+    loadWeightsButton.disabled = true;
+
+    status.innerHTML += "Starting testing ...<br>";
+
+    let correctPredicts = 0;
+  
+    testData.forEach((current, index) => {
+        setTimeout(() => {
+            const actual = testLabels[index];
+    
+            const predict = formatPrediction(net.predict(current));
+            predict === actual ? correctPredicts++ : null;
+
+            /* check if testing is complete */
+            if (index >= testData.length - 1) {
+                status.innerHTML += "Accuracy: " + Math.round((correctPredicts / testData.length) * 100) +" %<br><br>";
+
+            /* check if training is complete */
+            if (iter + 1 > iterations) {
+                createDownloadLink();
+                enableAllButtons();
+                status.innerHTML += "Finished training.<br><br>";
+                iter = 0;
+            } else if (inTraining) {
+            // if test is called from within training and the training is not complete yet, continue training
+                train();
+            } else {
+                enableAllButtons();
+            }
+            }  
+        }, 0);
+    });
+}
+
+function predict() {
+    /* resize the canvas to the training data image size  */
+    const tempCanvas = document.createElement("canvas");
+    const tempCtx = tempCanvas.getContext("2d");
+    tempCtx.drawImage(canvas, 0, 0, 150, 150, 0, 0, 28, 28);
+
+    /* convert the canvas image */
+    const img = tempCtx.getImageData(0, 0, 28, 28);
+
+    /* remove the alpha channel and convert to grayscale */
+    let sample = [];
+    for (let i = 0, j = 0; i < img.data.length; i += 4, j++) {
+        sample[j] = (img.data[i + 0] + img.data[i + 1] + img.data[i + 2]) / 3;
+    }
+
+    img.data = net.normalizeData(img.data);
+
+    const predict = formatPrediction(net.predict(sample));
+    status.innerHTML += "Prediction: " + predict + "<br>";
 }
 
 function formatPrediction(prediction) {
     const flattened = prediction.toArray().map((x) => x[0]);
     return flattened.indexOf(Math.max(...flattened));
+}
+
+function loadWeights() {
+    net.wInHidden = savedWeights.wInHidden;
+    net.wHiddenOut = savedWeights.wHiddenOut;
+    status.innerHTML += "Weights successfully loaded.<br>";
+}
+
+function createDownloadLink() {
+    const wInHidden = net.wInHidden.toArray();
+    const wHiddenOut = net.wHiddenOut.toArray();
+    const weights = {wInHidden, wHiddenOut};
+    download.innerHTML = `<a download="${weightsFilename}" id="downloadLink" href="data:text/json;charset=utf-8,${encodeURIComponent(
+        JSON.stringify(weights))}">Download model weights</a>`;
+}
+
+/* UI helper functions */
+
+function enableAllButtons() {
+  trainButton.disabled = false;
+  testButton.disabled = false;
+  loadWeightsButton.disabled = false;
 }
