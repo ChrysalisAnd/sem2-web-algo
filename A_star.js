19c82897220e123adb6bc3fbaf7ed0950dd21653
klazyt <tim.klimanov@gmail.com>
2023-04-14 09:37:09 +0700

    PureChaos
    Trying to start rhe algorythm

--- A_star.js
+++ A_star.js
@@ -1,7 +1,7 @@
 // Видео человека Conor Bailey https://www.youtube.com/watch?v=nHjqkLV_Tp0&t=721s
 let maze = document.querySelector(".maze");
 let ctx = maze.getContext('2d');
-const COLORS = ["#E5FCFF", "#FFD700", "#228B22", "#B22222"];
+const COLORS = ["#E5FCFF", "#FFD700", "#228B22", "#B22222", '#383e70'];
 let current;
 let inputSize = document.querySelector("#maze_size");
 
@@ -97,7 +97,7 @@ class Cell {
     ctx.fill();
   }
 }
-var newMaze = new Maze();
+let newMaze = new Maze();
 let mazeSize;
 inputSize.addEventListener("input", ()=>{
   mazeSize = parseInt(inputSize.value)
@@ -117,8 +117,75 @@ maze.addEventListener('click', function(e) {
   let { x, y } = getCursorPosition(maze, e);
   x = Math.floor(x / (newMaze.size / mazeSize));
   y = Math.floor(y / (newMaze.size / mazeSize))
-  console.log(x, y)
-  newMaze.grid[y][x].color = COLORS[(newMaze.grid[y][x].colorNum + 1) % 4];
-  newMaze.grid[y][x].colorNum += 1;
+  newMaze.grid[y][x].colorNum = (newMaze.grid[y][x].colorNum + 1) % 4;
+  newMaze.grid[y][x].color = COLORS[newMaze.grid[y][x].colorNum % 4];
   newMaze.grid[y][x].show(newMaze.size, newMaze.rows, newMaze.columns)
 })
+let startR = 0;
+let startC = 0;
+let finishR = 0
+let finishC = 0;
+for (let r = 0; r < mazeSize + 0; r++) {
+  for (let c = 0; c < mazeSize + 0; c++) {
+    if (newMaze.grid[r][c].colorNum == 2){
+      startR = r;
+      startC = c
+    }
+    if (newMaze.grid[r][c].colorNum == 3){
+      finishR = r;
+      finishC = c;
+    }
+  }
+}
+console.log(startR, startC, finishR, finishC)
+document.getElementById("clickMe").onclick = function() { SolveTheMaze(startR, startC, finishR, finishC) };
+
+function SolveTheMaze(startR, startC, finishR, finishC){
+  function checkPath(startR, startC, finishR, finishC) {
+    newMaze.grid[startR][startC].colorNum = 4;
+    let siblings = getValidSib();
+    if (siblings.length > 0) {
+      for (let i = 0; i < siblings.length; i++) {
+        let currentR = siblings[i][0];
+        let currentC = siblings[i][1];
+        let isSolved = currentR === finishR && current.C === finishC;
+        let notVisited = newMaze[currentR][currentC].colorNum !== 4;
+  
+        if (isSolved || (notVisited && checkPath(currentR, currentC, finishR, finishC))) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  
+  function getValidSib(R, C) {
+    let x = R
+    let y = C
+    let cords = [];
+    if (newMaze.grid[y - 1][x] !== undefined) {
+      cords.push({ x: x, y: y - 1, val: newMaze.grid[y - 1][x].colornum});
+    }
+    if (newMaze.grid[y + 1][x] !== undefined) {
+      cords.push({ x: x, y: y + 1, val: newMaze.grid[y + 1][x].colornum});
+    }
+    if (newMaze.grid[y][x - 1] !== undefined) {
+      cords.push({ x: x - 1, y: y, val: newMaze.grid[y][x - 1].colornum});
+    }
+    if (newMaze.grid[y][x + 1] !== undefined) {
+      cords.push({ x: x + 1, y: y, val: newMaze.grid[y][x + 1].colornum});
+    }
+  
+    return cords.filter((crd) => crd.val === 0);
+  }
+  if (checkPath(startR, startC, finishR, finishC)){
+    for (let r = 0; r < mazeSize + 0; r++) {
+      for (let c = 0; c < mazeSize + 0; c++) {
+        newMaze.grid[r][c].show(newMaze.size, newMaze.rows, newMaze.columns)
+      }
+    }
+
+  }
+  else
+    alarm("не повезло не фартануло")
+}
